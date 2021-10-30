5123f2e92199d7c4ba486a0319857cb6b57fca2b
Monmoy042 <monmoykms.16@gmail.com>
2021-10-31 05:48:17 +0600

    Truthy and Falsy value done

---
+++ 01truthy.js
@@ -0,0 +1,43 @@
+// Falsy Value
+// ===============
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+// Truthy Value
+// ===============
+// '0' ==> string
+// ' '
+// []
+// {}
+// true
+// 'false'
+
+// const age = 0;
+// if (age) {
+// 	console.log('condition is true');
+// } else {
+// 	console.log('Condition is false');
+// }
+
+// const name = 'Solaiman Khan';
+// const name = '';
+// const name = ' ';
+// const name = '0';
+// if (name) {
+// 	// if(name.length > 0){
+// 	console.log('Condition is true');
+// } else {
+// 	console.log('Condition is false');
+// }
+
+let name;
+console.log(name);
+if (name) {
+	console.log('Condition is true');
+} else {
+	console.log('Condition is false');
+}
