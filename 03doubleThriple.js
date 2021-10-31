95db70966c6703ce746708aa19ec7aacaa812540
Monmoy042 <monmoykms.16@gmail.com>
2021-10-31 06:18:02 +0600

    double equal vs triple equal done

---
+++ 03doubleThriple.js
@@ -0,0 +1,16 @@
+// Double Equal VS Triple Equal (== VS ===)
+// const first = 2;
+// const second = '2';
+
+// const first = 1;
+// const second = true;
+
+const first = 0;
+const second = false;
+
+if (first == second) {
+	// if (first === second) {
+	console.log('Condition is  true');
+} else {
+	console.log('Condition is false');
+}
