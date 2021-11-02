5772ca68e6b1415c9743d6ce089440dcc10cce47
Monmoy042 <monmoykms.16@gmail.com>
2021-11-02 23:31:31 +0600

    scope in JS practice done

---
+++ 06scope.js
@@ -0,0 +1,27 @@
+// Scope in JS
+let result;
+console.log(result);
+let bonus = 30;
+
+function sum(first, second) {
+	let result = first + second + bonus;
+	// result = first + second;
+	console.log(result);
+	console.log(bonus);
+	if (result > 9) {
+		// const mood = 'Happy';
+		let mood = 'Happy';
+		mood = 'Fishy';
+		mood = 'Funky';
+		console.log(mood);
+	}
+	// console.log(mood);
+    console.log(day);
+    var day = 'wednesday';
+	return result;
+}
+const output = sum(7, 5);
+console.log(result);
+console.log(output);
+
+// What is hoisting??
